39c2a97b6795e70c09a16fabf2206bbcc24ab553
Ansline Martiens <Ansline@gmail.com>
2025-07-21 23:13:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,17 @@
 
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp, getApps, getApp, FirebaseOptions } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-  projectId: "lifequest-cc4l7",
-  appId: "1:55049413123:web:6330cc4af7810eab12a436",
-  storageBucket: "lifequest-cc4l7.firebasestorage.app",
-  apiKey: "AIzaSyBoVr_1xTS13AirQ4u5BEnIROExxYNrV8c",
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "lifequest-cc4l7.firebaseapp.com",
+  projectId: "lifequest-cc4l7",
+  storageBucket: "lifequest-cc4l7.appspot.com",
   messagingSenderId: "55049413123",
+  appId: "1:55049413123:web:6330cc4af7810eab12a436",
 };
 
-// Initialize Firebase
+// Initialize Firebase for SSR
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
